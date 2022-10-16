d56b108a49f5267afd27494777958eaa5d61a948
mihson95 <sontakemihir@gmail.com>
2022-10-16 20:09:03 +0530

    lazy initialization

--- creational/singleton/com/dp/ConnectionPool.java
+++ creational/singleton/com/dp/ConnectionPool.java
@@ -2,15 +2,23 @@ package com.dp;
 public class ConnectionPool {
 
   //Step 3 - Add a static variable
-  private static ConnectionPool instance = new ConnectionPool();
+  private static ConnectionPool instance = null;
   
   //Step 1 - Hide constructor
   private ConnectionPool(){}
   
   //Step 2 - Static Initializer
   public static ConnectionPool getInstance() {
-        return instance;
+      if(instance == null) {
+		  synchronized (ConnectionPool.class) {
+	    	  if(instance == null) {
+	        	  instance = new ConnectionPool();
+	          }
+	      }
+      }
+	  return instance;
   }
 }
 
 //Eager initialization
+//Lazy initialization(on demand initialization)
